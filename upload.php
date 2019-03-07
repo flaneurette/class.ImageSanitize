0ac3fd0a8e2cbbc0845aa37f15f36626db596c80
Flaneurette <30347642+flaneurette@users.noreply.github.com>
2019-03-07 22:34:13 +0100

    Create upload.php

---
+++ upload.php
@@ -0,0 +1,31 @@
+<?php
+
+set_time_limit(0); 
+session_start(); 
+
+require("class.ImageSanitize.php");
+
+if(isset($_REQUEST['upload'])) {
+
+	$parameters = array('image' => 'files','path' => "test/",'thumb' => false,'width' => false,'height' => false);
+	$checkImage = new \security\images\ImageSecurityCheck($parameters);
+	$checkImage->fullScan(); 
+  var_dump($_FILES);
+  
+} 
+
+?>
+
+<h2>Secure image class</h2>
+
+<p>Select an image to process...</p>
+
+<form name="" action="" method="post" enctype="multipart/form-data"> 
+	<input type="file" name="files" /> 
+	<input type="hidden" name="upload" value="1" /> 
+	<input type="submit" name="submit" value="Upload Image" /> 
+</form>
+
+<small>NB. Don't forget to create a /test/ folder to write files to.</small>
+
+?>
